f634c819a1e57cba10d008359890e9d55143ab4e
Steven-P-Tran <93216433+Steven-P-Tran@users.noreply.github.com>
2022-05-08 20:41:07 -0700

    Initial push
    Did some firebase authentication and some general page routing

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import{getFirestore} from 'firebase/firestor';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBa787GELCHVbtO8yWj7qvkTOr6yM7bsXI",
+  authDomain: "house-marketplace-app-31ffc.firebaseapp.com",
+  projectId: "house-marketplace-app-31ffc",
+  storageBucket: "house-marketplace-app-31ffc.appspot.com",
+  messagingSenderId: "152750751301",
+  appId: "1:152750751301:web:6771181f114c3730f8ad2c"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFirestore();
