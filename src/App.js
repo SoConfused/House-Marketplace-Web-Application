3f8debd1aab58edea590c132fe46856369a53505
Steven-P-Tran <93216433+Steven-P-Tran@users.noreply.github.com>
2022-05-08 22:01:46 -0700

    Navbar creation
    Navbar creation was implemented with appropriate proportions and coloring. Colors may seem off

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
 import Profile from './pages/Profile'
@@ -18,10 +19,9 @@ function App() {
        <Route path='/sign-in' element={<SignIn />} />
        <Route path='/sign-up' element={<SignUp />} />
        <Route path='/forgot-password' element={<ForgotPassword />} />
-
      </Routes>
+        <Navbar />
     </Router>
-    {/*NavBar*/}
     </>
   )
 }
